aea10850b905e9ea45c59bd5c38924f5704ccb92
Jacob Hamblin <jacob@hamblin.cc>
2021-05-15 21:47:56 -0700

    cleanup

--- contracts/CritterOwnership.sol
+++ contracts/CritterOwnership.sol
@@ -1,11 +1,9 @@
 import "./tokens/nf-token.sol";
 
 contract CritterOwnership is CritterBase, ERC721 {
-    /// @notice Name and symbol of the non fungible token, as defined in ERC721.
     string public constant name = "CryptoCritters";
     string public constant symbol = "CK";
 
-    // The contract that will return critter metadata
     ERC721Metadata public erc721Metadata;
 
     bytes4 constant InterfaceSignature_ERC165 =
@@ -23,35 +21,19 @@ contract CritterOwnership is CritterBase, ERC721 {
             bytes4(keccak256("tokensOfOwner(address)")) ^
             bytes4(keccak256("tokenMetadata(uint256,string)"));
 
-    /// @notice Introspection interface as per ERC-165 (https://github.com/ethereum/EIPs/issues/165).
-    ///  Returns true for any standardized interfaces implemented by this contract. We implement
-    ///  ERC-165 (obviously!) and ERC-721.
     function supportsInterface(bytes4 _interfaceID)
         external
         view
         returns (bool)
     {
-        // DEBUG ONLY
-        //require((InterfaceSignature_ERC165 == 0x01ffc9a7) && (InterfaceSignature_ERC721 == 0x9a20483d));
-
         return ((_interfaceID == InterfaceSignature_ERC165) ||
             (_interfaceID == InterfaceSignature_ERC721));
     }
 
-    /// @dev Set the address of the sibling contract that tracks metadata.
-    ///  CEO only.
     function setMetadataAddress(address _contractAddress) public onlyCEO {
         erc721Metadata = ERC721Metadata(_contractAddress);
     }
 
-    // Internal utility functions: These functions all assume that their input arguments
-    // are valid. We leave it to public methods to sanitize their inputs and follow
-    // the required logic.
-
-    /// @dev Checks if a given address is the current owner of a particular
-    //Critter.
-    /// @param _claimant the address we are validating against.
-    /// @param _tokenId kitten id, only valid when > 0
     function _owns(address _claimant, uint256 _tokenId)
         internal
         view
@@ -60,10 +42,6 @@ contract CritterOwnership is CritterBase, ERC721 {
         return critterIndexToOwner[_tokenId] == _claimant;
     }
 
-    /// @dev Checks if a given address currently has transferApproval for a
-    //particular Critter.
-    /// @param _claimant the address we are confirming kitten is approved for.
-    /// @param _tokenId kitten id, only valid when > 0
     function _approvedFor(address _claimant, uint256 _tokenId)
         internal
         view
@@ -72,112 +50,56 @@ contract CritterOwnership is CritterBase, ERC721 {
         return critterIndexToApproved[_tokenId] == _claimant;
     }
 
-    /// @dev Marks an address as being approved for transferFrom(), overwriting any previous
-    ///  approval. Setting _approved to address(0) clears all transfer approval.
-    ///  NOTE: _approve() does NOT send the Approval event. This is intentional because
-    ///  _approve() and transferFrom() are used together for putting Critters on auction, and
-    ///  there is no value in spamming the log with Approval events in that case.
     function _approve(uint256 _tokenId, address _approved) internal {
         critterIndexToApproved[_tokenId] = _approved;
     }
 
-    /// @notice Returns the number of Critters owned by a specific address.
-    /// @param _owner The owner address to check.
-    /// @dev Required for ERC-721 compliance
     function balanceOf(address _owner) public view returns (uint256 count) {
         return ownershipTokenCount[_owner];
     }
 
-    /// @notice Transfers a Critter to another address. If transferring to a smart
-    ///  contract be VERY CAREFUL to ensure that it is aware of ERC-721 (or
-    ///  CryptoCritters specifically) or your Critter may be lost forever. Seriously.
-    /// @param _to The address of the recipient, can be a user or contract.
-    /// @param _tokenId The ID of the Critter to transfer.
-    /// @dev Required for ERC-721 compliance.
     function transfer(address _to, uint256 _tokenId) external whenNotPaused {
-        // Safety check to prevent against an unexpected 0x0 default.
         require(_to != address(0));
-        // Disallow transfers to this contract to prevent accidental misuse.
-        // The contract should never own any kitties (except very briefly
-        // after a gen0 cat is created and before it goes on auction).
         require(_to != address(this));
-        // Disallow transfers to the auction contracts to prevent accidental
-        // misuse. Auction contracts should only take ownership of kitties
-        // through the allow + transferFrom flow.
         require(_to != address(saleAuction));
         require(_to != address(siringAuction));
 
-        // You can only send your own cat.
         require(_owns(msg.sender, _tokenId));
 
-        // Reassign ownership, clear pending approvals, emit Transfer event.
         _transfer(msg.sender, _to, _tokenId);
     }
 
-    /// @notice Grant another address the right to transfer a specific Critter via
-    ///  transferFrom(). This is the preferred flow for transfering NFTs to contracts.
-    /// @param _to The address to be granted transfer approval. Pass address(0) to
-    ///  clear all approvals.
-    /// @param _tokenId The ID of the Critter that can be transferred if this call succeeds.
-    /// @dev Required for ERC-721 compliance.
     function approve(address _to, uint256 _tokenId) external whenNotPaused {
-        // Only an owner can grant transfer approval.
         require(_owns(msg.sender, _tokenId));
 
-        // Register the approval (replacing any previous approval).
         _approve(_tokenId, _to);
 
-        // Emit approval event.
         Approval(msg.sender, _to, _tokenId);
     }
 
-    /// @notice Transfer a Critter owned by another address, for which the calling address
-    ///  has previously been granted transfer approval by the owner.
-    /// @param _from The address that owns the Critter to be transfered.
-    /// @param _to The address that should take ownership of the Critter. Can be any address,
-    ///  including the caller.
-    /// @param _tokenId The ID of the Critter to be transferred.
-    /// @dev Required for ERC-721 compliance.
     function transferFrom(
         address _from,
         address _to,
         uint256 _tokenId
     ) external whenNotPaused {
-        // Safety check to prevent against an unexpected 0x0 default.
         require(_to != address(0));
-        // Disallow transfers to this contract to prevent accidental misuse.
-        // The contract should never own any kitties (except very briefly
-        // after a gen0 cat is created and before it goes on auction).
         require(_to != address(this));
-        // Check for approval and valid ownership
         require(_approvedFor(msg.sender, _tokenId));
         require(_owns(_from, _tokenId));
 
-        // Reassign ownership (also clears pending approvals and emits Transfer event).
         _transfer(_from, _to, _tokenId);
     }
 
-    /// @notice Returns the total number of Critters currently in existence.
-    /// @dev Required for ERC-721 compliance.
     function totalSupply() public view returns (uint256) {
         return kitties.length - 1;
     }
 
-    /// @notice Returns the address currently assigned ownership of a given
-    //Critter.
-    /// @dev Required for ERC-721 compliance.
     function ownerOf(uint256 _tokenId) external view returns (address owner) {
         owner = critterIndexToOwner[_tokenId];
 
         require(owner != address(0));
     }
 
-    /// @notice Returns a list of all Critter IDs assigned to an address.
-    /// @param _owner The owner whose Critters we are interested in.
-    /// @dev This method MUST NEVER be called by smart contract code. First, it's fairly
-    ///  expensive (it walks the entire Critter array looking for critters belonging to owner),
-    ///  but it also returns a dynamic array, which is only supported for web3 calls, and
-    ///  not contract-to-contract calls.
     function tokensOfOwner(address _owner)
         external
         view
@@ -186,15 +108,12 @@ contract CritterOwnership is CritterBase, ERC721 {
         uint256 tokenCount = balanceOf(_owner);
 
         if (tokenCount == 0) {
-            // Return an empty array
             return new uint256[](0);
         } else {
             uint256[] memory result = new uint256[](tokenCount);
             uint256 totalCats = totalSupply();
             uint256 resultIndex = 0;
 
-            // We count on the fact that all critters have IDs starting at 1 and increasing
-            // sequentially up to the totalCat count.
             uint256 catId;
 
             for (catId = 1; catId <= totalCats; catId++) {
@@ -208,15 +127,11 @@ contract CritterOwnership is CritterBase, ERC721 {
         }
     }
 
-    /// @dev Adapted from memcpy() by @arachnid (Nick Johnson <arachnid@notdot.net>)
-    ///  This method is licenced under the Apache License.
-    ///  Ref: https://github.com/Arachnid/solidity-stringutils/blob/2f6ca9accb48ae14c66f1437ec50ed19a0616f78/strings.sol
     function _memcpy(
         uint256 _dest,
         uint256 _src,
         uint256 _len
     ) private view {
-        // Copy word-length chunks while possible
         for (; _len >= 32; _len -= 32) {
             assembly {
                 mstore(_dest, mload(_src))
@@ -225,7 +140,6 @@ contract CritterOwnership is CritterBase, ERC721 {
             _src += 32;
         }
 
-        // Copy remaining bytes
         uint256 mask = 256**(32 - _len) - 1;
         assembly {
             let srcpart := and(mload(_src), not(mask))
@@ -234,9 +148,6 @@ contract CritterOwnership is CritterBase, ERC721 {
         }
     }
 
-    /// @dev Adapted from toString(slice) by @arachnid (Nick Johnson <arachnid@notdot.net>)
-    ///  This method is licenced under the Apache License.
-    ///  Ref: https://github.com/Arachnid/solidity-stringutils/blob/2f6ca9accb48ae14c66f1437ec50ed19a0616f78/strings.sol
     function _toString(bytes32[4] _rawBytes, uint256 _stringLength)
         private
         view
@@ -256,9 +167,6 @@ contract CritterOwnership is CritterBase, ERC721 {
         return outputString;
     }
 
-    /// @notice Returns a URI pointing to a metadata package for this token conforming to
-    ///  ERC-721 (https://github.com/ethereum/EIPs/issues/721)
-    /// @param _tokenId The ID number of the Critter whose metadata should be returned.
     function tokenMetadata(uint256 _tokenId, string _preferredTransport)
         external
         view
