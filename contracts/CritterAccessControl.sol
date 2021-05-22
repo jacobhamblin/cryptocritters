b6dbe97f50ba593cc25ed77b7d4d35efe9dc4582
Jacob Hamblin <jacob@hamblin.cc>
2021-05-22 15:29:15 -0700

    step back

--- contracts/CritterAccessControl.sol
+++ contracts/CritterAccessControl.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.4.11;
 
 contract CritterAccessControl {
     event ContractUpgrade(address newContract);
--- contracts/CritterOwnership.sol
+++ contracts/CritterOwnership.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.4.11;
 
 import "./tokens/nf-token.sol";
 import "./CritterBase.sol";
