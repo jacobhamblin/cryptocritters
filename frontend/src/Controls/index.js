02bbb4782646e4b7add4a139c919ab1c205a72dd
Jacob Hamblin <jacob@hamblin.cc>
2021-07-18 15:59:01 -0700

    style updates

--- frontend/src/Controls/index.js
+++ frontend/src/Controls/index.js
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import classNames from "classnames";
 import Web3 from "web3";
 import useInterval from "../utils/useInterval";
-import contractABI from "../abi/CritterOwnershipUpdated.json";
+import contractABIJSON from "../abi/CritterOwnershipUpdated.json";
 import "./style.css";
 
 const CONTRACT_ADDRESS = "0x51EF4dC2D7bBdb69031563B52cE26Ee28Bff84E7";
@@ -12,6 +12,14 @@ const Controls = ({ setCritters, setContract: setContractParent }) => {
   const [status, setStatus] = useState("");
   const [web3, setWeb3] = useState(null);
   const [contract, setLocalContract] = useState(null);
+
+  useEffect(() => {
+    if (!window.ethereum) {
+      setStatus("Please install metamask extension and reload page!");
+    } else {
+      setStatus("Metamask present, ready for blockchain transactions!");
+    }
+  }, []);
 
   const setContract = (contract) => {
     setLocalContract(contract);
@@ -49,18 +57,22 @@ const Controls = ({ setCritters, setContract: setContractParent }) => {
       console.log("connected");
       return;
     }
+    window.ethereum.request({ method: "eth_requestAccounts" });
 
     const web3 = new Web3(Web3.currentProvider);
     setWeb3(web3);
 
-    const contract = new web3.eth.Contract(contractABI, CONTRACT_ADDRESS);
+    const contract = new web3.eth.Contract(
+      contractABIJSON.abi,
+      CONTRACT_ADDRESS
+    );
     setContract(contract);
   };
 
   const accountText = accountID ? accountID : "Connect Metamask";
   return (
     <div className="Controls">
-      <div className="transactionStatus nes-text">{status}</div>
+      <span className="transactionStatus nes-text">{status}</span>
       <div className="buttons">
         <button
           className={classNames({
